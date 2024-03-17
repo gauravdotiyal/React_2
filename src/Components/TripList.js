56f3d9b9aee1d5ea3383f7f24858ed2880c9fe41
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-03-18 00:20:31 +0530

    new hook useCallback function

--- src/Components/TripList.js
+++ src/Components/TripList.js
@@ -1,18 +1,44 @@
-import { useState } from "react"
+import { useState,useEffect,useCallback } from "react"
+import './TripList.css'
+ export default function TripList() {  
 
- export default function TripList() { 
     const [trips,setTrips]=useState([])
-    
+    const [url,setUrl]=useState('http://localhost:3000/trips')
+
+    const fetchtrips= useCallback( async()=>{
+      const response=await fetch(url)
+      const json =  await response.json()
+      setTrips(json)
+    },[url])
+
+    useEffect(()=>{
+         fetchtrips()
+    },[fetchtrips])
+
     // console.log(trips)
-
-    fetch('http://localhost:3000/trips')
-    .then(response=> response.json())
-    .then(json=>setTrips(json))
-
-
+  
   return (
-    <div>
+    <div className="trip-list">
       <h2>Trip List</h2>
+      <ul>
+        {trips.map(trip=>(
+            <li key={trip.id}>
+                <h3>{trip.title}</h3>
+                <p>{trip.price}</p>
+                {/* <p>{trip.loc}</p> */}
+            </li>
+        ))}
+      </ul>
+
+      <div className="filter">
+        <button onClick={()=>setUrl('http://localhost:3000/trips?loc=Europe')}>
+            European Trips
+        </button >
+        <button onClick={()=>setUrl('http://localhost:3000/trips')}>
+            All the trips
+        </button>
+
+      </div>
     </div>
   )
 }
