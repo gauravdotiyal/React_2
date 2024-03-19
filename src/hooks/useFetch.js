74dce3d8f573791895fb55ea338848b01a3eb7bd
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-03-20 02:07:00 +0530

    error handling, cleanup function

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,50 @@
-import { useState,useEffect } from "react"
+import { useState,useEffect,useRef } from "react"
 
 
-export const useFetch =(url)=>{
+export const useFetch =(url,_options)=>{
    const [data,setData]=useState(null)
-     
-   useEffect(()=>{
-       const fetchData=async()=>{
-          const res=await fetch(url)
-          const json =await res.json()
+   const [isPending,setIsPending]=useState(false)
+   const [error,setError]=useState(null)
+   
+   //use useRef to wrap an object/array argument 
+   //which is useEffect dependency
+   const options=useRef(_options).current
 
-          setData(json)
+   useEffect(()=>{
+       console.log(options)
+       const controller=new AbortController()
+       const fetchData=async()=>{
+        setIsPending(true)
+          
+          try{
+            const res=await fetch(url,{signal:controller.signal})
+            if(!res.ok){
+               throw new Error(res.statusText)
+            }
+            console.log(res)    
+            const json =await res.json()
+          
+            setIsPending(false)
+            setData(json)
+            setError(null)
+          }catch(err){
+            if(err==="AbortError"){
+                console.log("Fetch is aborted ")
+            }
+            else {
+                setIsPending(false)
+                setError('could not fetch the data') 
+            }  
+          }
+          
        }
 
        fetchData()
+
+    return ()=>{
+       controller.abort()
+    }
    },[url])
 
-   return {data}
+   return {data,isPending,error}
 }  
